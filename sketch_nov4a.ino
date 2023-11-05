bd0db3caa5f954885bfb7bbd3e0455e29b2e7e8d
Bhagyashree-Ratnakant-Yakkundi <bhagyashreeyakkundi@gmail.com>
2023-11-05 13:00:46 +0530

    my first commit

---
+++ sketch_nov4a.ino
@@ -0,0 +1,17 @@
+int  led=42;
+void setup() {
+  pinMode(led,OUTPUT);
+
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led,HIGH);
+delay(1000);
+digitalWrite(led,LOW);
+delay(1000);
+}
+
+
